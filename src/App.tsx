f85cf2aea5a67cda482cc75938e5ac947c700e9d
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-09-14 15:16:50 -0300

    update landing page
    add cards, grid cards ads, logo, background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import './styles/main.css'
+import './styles/app.css'
+import logoImg from './assets/Logo.svg'
+import { GridAds } from './components/gridAds'
+import { MagnifyingGlassPlus} from 'phosphor-react'
+
 function App() {
 
   return (
     <div className="App">
-      Hello Tiago
+      <img src={logoImg} alt="" />
+      <h1 className='title'>Seu <span className='gradient'>duo</span> está aqui.</h1>
+
+      <GridAds />
+      <div className='gradientBorder'>
+        <div className='container-PublishBox'>
+          <div>
+            <strong>Não encontrou seu duo?</strong>
+            <span>Public um anúncio para encontrar  novos players!</span>
+          </div>
+          <button className='btn-publish'>
+          <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+            </button>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/cardAds/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+
+interface CardsAdsProps {
+    title?: string,
+    imgSrc: string,
+    quantAds: string
+}
+
+export function CardAds(props: CardsAdsProps) {
+    const {title, imgSrc, quantAds} = props
+  return (
+    <a className='container-CardAds'>
+        <img src={imgSrc} alt="" />
+
+        <div className='text-Box'>
+            <strong>{title}</strong>
+            <span>{quantAds}</span>
+        </div>
+    </a>
+  )
+}
